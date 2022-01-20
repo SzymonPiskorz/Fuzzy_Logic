6bf4c77c7e7e4abdf0c847f5c59a725892444776
SzymonPiskorz <c00242808@itcarlow.ie>
2022-01-20 15:57:27 +0000

    First commit

---
+++ Fuzzy_Logic/include/Game.h
@@ -0,0 +1,35 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+/// <summary>
+/// include guards used so we don't process this file twice
+/// same as #pragma once
+/// Don't forget the endif at the bottom
+/// </summary>
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	/// <summary>
+	/// main method for game
+	/// </summary>
+	void run();
+
+private:
+
+	void processEvents();
+	void processKeys(sf::Event t_event);
+	void update(sf::Time t_deltaTime);
+	void render();
+
+	void setupFontAndText();
+
+	sf::RenderWindow m_window; // main SFML window
+	sf::Font m_ArialBlackfont; // font used by message
+	bool m_exitGame; // control exiting game
+};
+
+#endif // !GAME_HPP
+
